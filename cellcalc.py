3e50e6eb1a954e3577d06b4fff0e4fc2f14bb4cf
joh-mue <yesiamkeen@gmail.com>
2017-04-11 15:48:05 +0200

    cellcalc prototype works

--- cellcalc.py
+++ cellcalc.py
@@ -3,39 +3,61 @@ Python Cell Calculator for Matrix Multiplication
 '''
 import boto3
 import json
+import pickle
+
+s3_client = boto3.client('s3')
 
 def handler(event, context):
-  column = event['column']
-  row = event['row']
   matrix_a = event['matrix_a']
   matrix_b = event['matrix_b']
+  column = event['column'] - 1
+  row = event['row'] - 1
   print "I got" + ", " + str(column) + ", " + str(row) + ", " + matrix_a + ", " + matrix_b
 
-  matrix_data = load_matrices(matrix_a, matrix_b)
-  result = multiply_matrices(matrix_data[0],matrix_data[1])
+  matrix_a_data = load_matrix(matrix_a)
+  matrix_b_data = load_matrix(matrix_b)
+  
+  cell = calculate_cell(row, column, matrix_a_data, matrix_b_data)
+  print cell
+  
+  bucket = event['target']
+  key = "C_" + str(column) + "-" + str(row)
+  write_to_s3(bucket, key, cell)
 
   response = {
-    "cell": 0,
-    "target": "/path"
-  };
+    "cell": cell,
+    "target": bucket
+  }
 
   return response
 
-def load_matrices(matrix_a, matrix_b):
-  matrix_a_data = [[1,2,3],[4,5,6],[7,8,9]]
-  matrix_b_data = [[1,2,3],[4,5,6],[7,8,9]]
-  return matrix_a_data, matrix_b_data
+def load_matrix(matrix):
+  bucketName = 'jmue-matrixes'
+  s3_client.download_file(bucketName, matrix, '/tmp/' + matrix)
 
-def multiply_matrices(matrix_a, matrix_b):
-  for row in range(len(matrix_a)):  
-    for column in range(len(matrix_a[0])):
-      cell = calculate_cell(row,column, matrix_a, matrix_b)
-      print "[",cell,"]",
-    print ""
-  return []
+  with open('/tmp/' + matrix, "rb") as file:
+    matrix_data = pickle.load(file)
+  return matrix_data
 
 def calculate_cell(row,column, matrix_a, matrix_b):
   cell = 0
   for y in range(len(matrix_a[0])):
     cell += matrix_a[row][y] * matrix_b[y][column]
   return cell
+
+def write_to_s3(bucket, key, result):
+  with open("/tmp/cell.data", 'wb') as file:
+    pickle.dump(result, file)
+  s3_client.upload_file('/tmp/cell.data', 'jmue-matrixes', key)
+
+
+'''
+event
+{
+  matrix_a: "matrix_A001",
+  matrib_b: "matrix_B001",
+  row: 1, use matrix notation
+  column: 1, use matrix notation
+  target: "jmue-matrixes"
+}
+'''
--- matrixmultiply.py
+++ matrixmultiply.py
@@ -7,6 +7,40 @@ import json
 lambda_client = boto3.client('lambda')
 
 def handler(event, context):
+  matrix_a = 
+  matrix_b = 
+
+  load_matrices(matrix_a, matrix_b)
+
+  for row in range(len(matrix_a)):  
+    for column in range(len(matrix_a[0])):
+      # calculate cell
+      cell = calculate_cell(row,column, matrix_a, matrix_b)
+      # invoke the reducers asynchronously
+      resp = lambda_client.invoke( 
+              FunctionName = r_function_name,
+              InvocationType = 'Event',
+              Payload =  json.dumps({
+                  "bucket": bucket,
+                  "keys": batch,
+                  "jobBucket": bucket,
+                  "jobId": job_id,
+                  "nReducers": n_reducers, 
+                  "stepId": step_id, 
+                  "reducerId": i 
+              })
+          )
+      print resp
+      print "[",cell,"]",
+    print ""
+  return []
+
+def load_matrices(matrix_a, matrix_b):
+  matrix_a_data = [[1,2,3],[4,5,6],[7,8,9]]
+  matrix_b_data = [[1,2,3],[4,5,6],[7,8,9]]
+  return matrix_a_data, matrix_b_data
+
+def multiply_matrices(matrix_a, matrix_b):
   for row in range(len(matrix_a)):  
     for column in range(len(matrix_a[0])):
       cell = calculate_cell(row,column, matrix_a, matrix_b)
