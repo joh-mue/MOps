4a0919fd6f4df5f86fc0bdc792b240f5c9c25be0
joh-mue <yesiamkeen@gmail.com>
2017-04-13 00:27:20 +0200

    fixed timeouts, lambda invocation

--- parallel-multiplication.py
+++ parallel-multiplication.py
@@ -4,8 +4,8 @@
   "matrix-a-key": "matrix-a-key",
   "matrix-b-key": "matrix-b-key",
   "matrix-c-key": "matrix-c-key",
-  "matrix-a-size": "matrix-a-size",
-  "matrix-b-size": "matrix-b-size"
+  "matrix-a-size": matrix-a-size,
+  "matrix-b-size": matrix-b-size
 }
 '''
 
@@ -13,18 +13,15 @@ import boto3
 import json
 import pickle
 
-s3_client = boto3.client('s3')
+lambda_client = boto3.client('lambda')
 
 def handler(event, context):
   bucket = event['bucket']
   matrix_a_key = event['matrix-a-key']
   matrix_b_key = event['matrix-b-key']
   matrix_c_key = event['matrix-c-key']
+  matrix_a_size = event['matrix-a-size']
 
-  multiply(matrix_a_key, matrix_b_key, matrix_c_key)
-
-
-def multiply(matrix_a_key, matrix_b_key, matrix_c_key):
   result_size = matrix_a_size
   for row in range(result_size):
     for column in range(result_size):
@@ -33,12 +30,11 @@ def multiply(matrix_a_key, matrix_b_key, matrix_c_key):
 def calculate_cell(row, column, matrix_a_key, matrix_b_key, matrix_c_key, bucket):
   response = lambda_client.invoke(
       FunctionName="mmultiply-prod-cellcalc",
-      InvocationType='RequestResponse',
-      LogType='Tail',
+      InvocationType='Event',
       Payload=json.dumps({
           "matrix-a-key": matrix_a_key,
           "matrix-b-key": matrix_b_key,
-          "matrix-c-key": matrix_c_key,,
+          "matrix-c-key": matrix_c_key,
           "row": row,
           "column": column,
           "bucket": bucket
