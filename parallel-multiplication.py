d25c71a6222942e3aba26e88fea788d5b13688b2
joh-mue <yesiamkeen@gmail.com>
2017-04-12 23:56:19 +0200

    implemented parallel-multiplication

---
+++ parallel-multiplication.py
@@ -0,0 +1,46 @@
+'''
+{
+  "bucket": "bucket-name",
+  "matrix-a-key": "matrix-a-key",
+  "matrix-b-key": "matrix-b-key",
+  "matrix-c-key": "matrix-c-key",
+  "matrix-a-size": "matrix-a-size",
+  "matrix-b-size": "matrix-b-size"
+}
+'''
+
+import boto3
+import json
+import pickle
+
+s3_client = boto3.client('s3')
+
+def handler(event, context):
+  bucket = event['bucket']
+  matrix_a_key = event['matrix-a-key']
+  matrix_b_key = event['matrix-b-key']
+  matrix_c_key = event['matrix-c-key']
+
+  multiply(matrix_a_key, matrix_b_key, matrix_c_key)
+
+
+def multiply(matrix_a_key, matrix_b_key, matrix_c_key):
+  result_size = matrix_a_size
+  for row in range(result_size):
+    for column in range(result_size):
+      calculate_cell(row, column, matrix_a_key, matrix_b_key, matrix_c_key, bucket)
+
+def calculate_cell(row, column, matrix_a_key, matrix_b_key, matrix_c_key, bucket):
+  response = lambda_client.invoke(
+      FunctionName="mmultiply-prod-cellcalc",
+      InvocationType='RequestResponse',
+      LogType='Tail',
+      Payload=json.dumps({
+          "matrix-a-key": matrix_a_key,
+          "matrix-b-key": matrix_b_key,
+          "matrix-c-key": matrix_c_key,,
+          "row": row,
+          "column": column,
+          "bucket": bucket
+      })
+  )
