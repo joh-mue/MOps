4c01282230405843d40b9d7417e88df88191e5e6
joh-mue <yesiamkeen@gmail.com>
2017-06-22 09:33:06 +0200

    implement lambda-strassen, fix package includes

--- strassen.py
+++ strassen.py
@@ -1,11 +1,15 @@
 '''
 event[''
 {
-  "method": "m_0",
+  "op": "intermediate",
+  "intermediate": "m_0", 
   "matrix-X": "/strassen-test/X",
   "matrix-Y": "/strassen-test/Y",
-  "bucket": "bucket-name"
+  "result": "result-bucket"
 }
+
+methods: m_X, strassen, intermediate ,collect
+
 '''
 
 import boto3
@@ -16,13 +20,14 @@ import pickle
 import os
 import ctypes
 
-for d, _, files in os.walk('lib'):
-    for f in files:
-        if f.endswith('.a'):
-            continue
-        ctypes.cdll.LoadLibrary(os.path.join(d, f))
+import platform
+if platform.system() != 'Darwin': # don't do this on my local machine
+  for d, _, files in os.walk('lib'):
+      for f in files:
+          if f.endswith('.a'):
+              continue
+          ctypes.cdll.LoadLibrary(os.path.join(d, f))
 
-import sklearn
 import numpy as np
 ### NUMPY, SCIPY, SKLEARN MAGIC END
 
@@ -30,31 +35,48 @@ lambda_client = boto3.client('lambda')
 s3_client = boto3.client('s3')
 
 def handler(event, context):
-  matX = load_matrix(event['matrix-X'])
-  matY = load_matrix(event['matrix-Y'])
-  # method = globals()[event['method']]
+  if event['op'] == 'strassen-local':
+    matX = load_matrix(event['matrix-X'])
+    matY = load_matrix(event['matrix-Y'])
 
-  intermediates = ["m_" + str(x) for x in range(0,7)]
-  m = []
-  for intermediate in intermediates:
-    intermediate_method = globals()[intermediate]
-    m.append(intermediate_method(matX, matY.transpose()))
-    # print(partial)
+    intermediates = ["m_" + str(x) for x in range(0,7)]
+    m = []
+    for intermediate in intermediates:
+      intermediate_method = globals()[intermediate]
+      m.append(intermediate_method(matX, matY.transpose()))
 
-  Q00 = m[0] + m[3] - m[4] + m[6]
-  Q01 = m[2] + m[4]
-  Q10 = m[1] + m[3]
-  Q11 = m[0] + m[2] - m[1] + m[5]
+    Q00 = m[0] + m[3] - m[4] + m[6]
+    Q01 = m[2] + m[4]
+    Q10 = m[1] + m[3]
+    Q11 = m[0] + m[2] - m[1] + m[5]
 
-  result = np.array([np.append(Q00[0], Q01[0]),
-  np.append(Q00[1], Q01[1]),
-  np.append(Q10[0], Q11[0]),
-  np.append(Q10[1], Q11[1])])
+    # TODO: use np.concatenate? (https://stackoverflow.com/a/9775378/1619878)
+    result = np.array([np.append(Q00[0], Q01[0]),
+                       np.append(Q00[1], Q01[1]),
+                       np.append(Q10[0], Q11[0]),
+                       np.append(Q10[1], Q11[1])])
 
-  print(result)
-  return { "Strassen correct": np.array_equal(result, matX.dot(matX.transpose())) }
+    print(result)
+    return { "Strassen correct": np.array_equal(result, matX.dot(matX.transpose())) }
 
-  # write_to_s3(data, bucket, key, context)
+  elif event['op'] == 'strassen':
+    intermediates = ["m_" + str(x) for x in range(0,7)]
+    for intermediate in intermediates:
+      trigger_intermediate(intermediate)
+
+  elif event['op'] == 'intermediate':
+    intermediate_method = globals()[event['intermediate']]
+    matX = load_matrix(event['matrix-X'])
+    matY = load_matrix(event['matrix-Y'])
+    result = intermediate_method(matX, matY.transpose())
+    print result
+    # write_to_s3(data, bucket, key, context)
+
+  elif event['op'] == 'collect':
+    print("empty")
+
+  else:
+    return { "Error": "invalid op code"}
   
   '''
   SYNTAX for modules:
@@ -63,14 +85,33 @@ def handler(event, context):
   result = method_to_call()
   '''
 
+def trigger_intermediate(intermediate):
+  response = lambda_client.invoke(
+    FunctionName="mmultiply-prod-strassen",
+    InvocationType='RequestResponse',
+    LogType='Tail',
+    Payload=json.dumps({
+      "op": "intermediate",
+      "intermediate": intermediate,
+      "matrix-X": "/strassen-test/X",
+      "matrix-Y": "/strassen-test/Y",
+      "result": "result-bucket"
+    })
+  )
+
 def load_matrix(matrix):
-  return np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+  if matrix == 'dummy':
+    return np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+  else:
+    # TODO: use download_fileobj so no file has to be written
+    s3_client.download_file('jmue-matrix-tests', matrix, '/tmp/' + matrix)
+    return np.load('/tmp/' + matrix)
 
 def split4(array, col, row):
   cols, rows = array.shape
   return array[(cols/2)*col:(cols+cols*col)/2, (rows/2)*row:(rows+rows*row)/2]
 
-# INTERMEDIATE CALCULATIONS
+# INTERMEDIATE CALCULATIONs
 
 def m_0(X, Y):
   return (split4(X,0,0) + split4(X,1,1)).dot(split4(Y,0,0) + split4(Y,1,1))
