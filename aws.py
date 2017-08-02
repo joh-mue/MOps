bf34c39961f2f8d61deeca9f962f292af4520173
joh-mue <yesiamkeen@gmail.com>
2017-08-02 21:56:00 +0200

    implemented unit-accumulator, changed naming scheme

--- aws.py
+++ aws.py
@@ -11,11 +11,11 @@ def write_to_s3(data, bucket, folder, key, s3_client):
 
   s3_client.upload_file(tmp_filepath, bucket, folder + "/" + key)
 
-def download_s3_file(bucket, folder, filename, s3_client):
-  key = folder + "/" + filename # e.g. m_0_0.npy
 
+def download_s3_file(bucket, folder, filename, s3_client):
   if not os.path.exists('/tmp/' + folder):
     os.mkdir('/tmp/' + folder)
   
+  key = folder + "/" + filename # e.g. m_0_0.npy
   s3_client.download_file(bucket, key, '/tmp/' + key)
   return '/tmp/' + key
---
+++ unit-accumulator.py
@@ -0,0 +1,68 @@
+import boto3
+import json
+import aws
+
+### NUMPY, SCIPY, SKLEARN MAGIC
+import os
+import ctypes
+
+import platform
+if platform.system() != 'Darwin': # don't do this on my local machine
+  for d, _, files in os.walk('lib'):
+      for f in files:
+          if f.endswith('.a'):
+              continue
+          ctypes.cdll.LoadLibrary(os.path.join(d, f))
+
+import numpy as np
+### NUMPY, SCIPY, SKLEARN MAGIC END
+
+s3_client = boto3.client('s3')
+
+# HANDLERS
+
+'''
+accumulate.json
+{
+  "split": 3
+}
+'''
+def accumulate(event, context):
+  for block_index in ["00","01","10","11"]:
+    partial_paths = load_all_partials(block_index, event['result'], event['split'])
+    
+    # add them up
+    final_block = np.zeros(shape=(1000,1000), dtype=np.int)
+    for path in partial_paths:
+      final_block += np.load(path)
+
+    index = get_absolute_block_index(block_index, event['result']['split'])
+
+    aws.write_to_s3(
+      data=final_block,
+      bucket=event['result']['bucket'],
+      folder=event['result']['folder'],
+      key="X{}{}".format(index[0], index[1]), #TODO absolute not relative block_index needed here
+      s3_client=s3_client
+    )
+
+def load_all_partials(block_index, result, split):
+  response = s3_client.list_objects_v2(
+      Bucket=result['bucket'],
+      Prefix="{}/S{}_X{}".format(result['folder'], split, block_index)
+    )
+
+  paths = []
+  for item in response['Contents']:
+    local_path = '/tmp/' + item['Key']
+    s3_client.download_file(result['bucket'], item['Key'], local_path)
+    paths.append(local_path)
+
+  return paths
+
+def get_absolute_block_index(block_index, split):
+  x, y = int(block_index[0]), int(block_index[1])
+  x += split['x1']/1000
+  y += split['y1']/1000
+  partition_factor = ((split['x2']-split['x1'])/2)/1000 # length of split us twice the size of a partition
+  return (partition_factor*x, partition_factor*y)
