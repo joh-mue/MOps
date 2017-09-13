64704e5b6b64591b963ec16e1288fa108387e1f5
joh-mue <yesiamkeen@gmail.com>
2017-09-13 20:46:20 +0200

    benchmark checks if matrices are deployed already

--- benchmarks/time-profiling.py
+++ benchmarks/time-profiling.py
@@ -34,7 +34,7 @@ sfn_client = boto3.client('stepfunctions')
 
 def _log(message):
     timestamp = datetime.now().strftime('%d.%m.%y %H:%M:%S > ')
-    LOG_FILE.writelines([timestamp + message])
+    LOG_FILE.write(timestamp + message + '\n')
     print timestamp + message
 
 
@@ -77,15 +77,23 @@ def upload_blocks(matrix_name, bucket):
     for filename in filenames:
         _log('Uploading to s3: {}'.format(filename))
         s3_client.upload_file(
-            Filename=os.path.join(localpath,filename),
-            Bucket=bucket,
-            Key=os.path.join(s3_folder,filename)
-        )
+                Filename=os.path.join(localpath,filename),
+                Bucket=bucket,
+                Key=os.path.join(s3_folder,filename))
 
 def deploy_matrices(matrix_names):
     for matrix_name in matrix_names:
         _log('Uploading matrix to s3: {}'.format(matrix_name))
         upload_blocks(matrix_name, BUCKET)
+
+
+
+def deployed(matrix_names, bucket, matrix_dimensions, block_size):
+    for matrix_name in matrix_names:
+        key_count = s3_client.list_objects_v2(Bucket=BUCKET, Prefix=matrix_name)['KeyCount']
+        if key_count != (matrix_dimensions.height/block_size * matrix_dimensions.width/block_size):
+            return False # this one is not deployed yet
+    return True
 
 
 
@@ -118,11 +126,10 @@ def invoke_matrix_multiplication(state_machine_name, executionName, name_matrixA
     sfn_input = create_input(state_machine_name, executionName, name_matrixA, name_matrixB)
     _log("Invoking matrix multiplication with input: {}".format(sfn_input))
     response = lambda_client.invoke(
-        FunctionName='mmultiply-prod-multi-unit-multiplication',
-        InvocationType='RequestResponse',
-        LogType='Tail',
-        Payload=json.dumps(sfn_input)
-    )
+            FunctionName='mmultiply-prod-multi-unit-multiplication',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            Payload=json.dumps(sfn_input))
     _log('LogResult: \n{}'.format(base64.b64decode(response['LogResult'])))
     return json.loads(response['Payload'].read())
 
@@ -133,13 +140,12 @@ def executions_pending(executionARNs):
     Failed executions are skipped. """
     for executionARN in executionARNs:
         last_event = sfn_client.get_execution_history(
-            executionArn=executionARN,
-            maxResults=1,
-            reverseOrder=True
-        )['events'][0]
+                executionArn=executionARN,
+                maxResults=1,
+                reverseOrder=True)['events'][0]
 
         if last_event['type'] == 'ExecutionFailed':
-            _log('Execution {} failed.'.format(executionARN))
+            _log('[ERROR] Execution {} failed.'.format(executionARN))
             pass
         elif last_event['type'] != 'ExecutionSucceeded':
             return True # Executions still pending
@@ -170,9 +176,8 @@ def get_all_events(executionARNs):
         while response.has_key('nextToken'):
             _log('Loading further events')
             response = sfn_client.get_execution_history(
-                executionArn=executionARN,
-                nextToken=response['nextToken']
-            )
+                    executionArn=executionARN,
+                    nextToken=response['nextToken'])
             events.extend(response['events'])
 
     return events
@@ -260,7 +265,7 @@ BLOCK_SIZE = 1000
 BUCKET = 'jmue-multiplication-benchmarks'
 PREFIX = 'sq'
 BENCHMARKS_FOLDER = '/Users/Johannes/Uni/Master/Master Arbeit/repos/matrix-operations/benchmarks/'
-SFN_PREFIX = 'create-refactored-test'
+SFN_PREFIX = 'skip-upload-test-2'
 
 ########################
 ###   Main Programm  ###
@@ -276,11 +281,11 @@ _log('benchmark parameters:\n  BLOCK_SIZE:{}\n  BUCKET:{}\n  PREFIX:{}\n  BENCHM
                                 BLOCK_SIZE, BUCKET, PREFIX, BENCHMARKS_FOLDER, SFN_PREFIX))
 
 matrix_dimension_sets = [
-        MatrixDimensions(height=2000, width=2000),
-        # MatrixDimensions(height=3000, width=3000),
+        # MatrixDimensions(height=2000, width=2000),
+        MatrixDimensions(height=3000, width=3000),
         # MatrixDimensions(height=4000, width=4000),
         # MatrixDimensions(height=8000, width=8000)
-]
+        ]
 
 _log(str(matrix_dimension_sets))
 
@@ -288,17 +293,20 @@ for matrix_dimensions in matrix_dimension_sets:
     matrix_name = '{}_{}kx{}k'.format(PREFIX, matrix_dimensions.height/1000, matrix_dimensions.width/1000)
     _log('Benchmarking {}'.format(matrix_name))
 
+    matrix_names = [matrix_name, matrix_name+'-2']
+    create_matrices(matrix_dimensions, matrix_names)
     
-    create_matrices(matrix_dimensions, matrix_names=[matrix_name, matrix_name+'-2'])
-    deploy_matrices(matrix_names=[matrix_name, matrix_name+'-2'])
+    if not deployed(matrix_names, BUCKET, matrix_dimensions, BLOCK_SIZE):
+        deploy_matrices(matrix_names)
+    else:
+        _log("Both matrices {} appear to be uploaded to S3. Skipping deployment.".format(matrix_names))
 
     state_machine_name = '{}-{}kx{}k'.format(SFN_PREFIX, matrix_dimensions.height/1000, matrix_dimensions.width/1000)
     execution_info = invoke_matrix_multiplication(
             state_machine_name=state_machine_name,
             executionName=state_machine_name,
             name_matrixA=matrix_name,
-            name_matrixB=matrix_name + '-2'
-    )
+            name_matrixB=matrix_name + '-2')
     split_executionARNs = [x['executionARN'] for x in execution_info['split-executions']]
     _log("Waiting for pending executions to finish...")
     while executions_pending(split_executionARNs):
