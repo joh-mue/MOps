f51ad9c9531af068bcdb47e909500874ba5f6359
joh-mue <yesiamkeen@gmail.com>
2017-10-13 13:50:49 +0200

    rearranged code benchmark code

--- benchmarks/time_profiling.py
+++ benchmarks/time_profiling.py
@@ -44,31 +44,6 @@ def _log(message):
 
 
 
-def write_block_to_file(block, directory, row, column):
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-    path = join(directory, 'm_{}_{}'.format(row, column))
-    _log('Writing block to file: {}'.format(path))
-    np.save(path, block)
-
-def get_block_with_random(height, width):
-    return np.random.random_sample((height,width))
-
-def create_blocks_with_random(x, y, block_height, block_width, matrix_name):
-    for i in range(0,x):
-        for j in range(0,y):
-            block = get_block_with_random(block_height, block_width)
-            directory = join(DATA_DIR, matrix_name)
-            write_block_to_file(block, directory, i, j)
-
-def created(matrix_name, matrix_dimensions, block_size):
-    if not os.path.exists(join(DATA_DIR, matrix_name)):
-        os.mkdir(join(DATA_DIR, matrix_name))
-        return False
-    else:
-        file_count = len(os.listdir(join(DATA_DIR, matrix_name)))
-        return file_count == (matrix_dimensions.height/block_size * matrix_dimensions.width/block_size)
-
 def create_matrix(matrix_name, matrix_dimensions, block_size):
     if not created(matrix_name, matrix_dimensions, block_size):
         x = matrix_dimensions.height/block_size
@@ -78,11 +53,32 @@ def create_matrix(matrix_name, matrix_dimensions, block_size):
     else:
         _log("[WARNING]Matrix {} appears to be created already. Skipping creation.".format(matrix_name))
 
+def created(matrix_name, matrix_dimensions, block_size):
+    if not os.path.exists(join(DATA_DIR, matrix_name)):
+        os.mkdir(join(DATA_DIR, matrix_name))
+        return False
+    else:
+        file_count = len(os.listdir(join(DATA_DIR, matrix_name)))
+        return file_count == (matrix_dimensions.height/block_size * matrix_dimensions.width/block_size)
+
+def create_blocks_with_random(x, y, block_height, block_width, matrix_name):
+    for i in range(0,x):
+        for j in range(0,y):
+            block = get_block_with_random(block_height, block_width)
+            directory = join(DATA_DIR, matrix_name)
+            write_block_to_file(block, directory, i, j)
+
+def get_block_with_random(height, width):
+    return np.random.random_sample((height,width))
+
+def write_block_to_file(block, directory, row, column):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    path = join(directory, 'm_{}_{}'.format(row, column))
+    _log('Writing block to file: {}'.format(path))
+    np.save(path, block)
 
 
-def deployed(matrix_name, bucket):
-    key_count = s3_client.list_objects_v2(Bucket=bucket, Prefix=matrix_name+'/')['KeyCount']
-    return key_count == len(os.listdir(join(DATA_DIR, matrix_name)))
 
 def deploy_matrix(matrix_name, bucket):
     if not deployed(matrix_name, bucket):
@@ -99,24 +95,35 @@ def deploy_matrix(matrix_name, bucket):
     else:
         _log("[WARNING]Matrix {} appears to be uploaded to S3. Skipping deployment.".format(matrix_name))
 
+def deployed(matrix_name, bucket):
+    key_count = s3_client.list_objects_v2(Bucket=bucket, Prefix=matrix_name+'/')['KeyCount']
+    return key_count == len(os.listdir(join(DATA_DIR, matrix_name)))
 
 
-def executions_pending(executionARNs):
-    """ Checks if all executions have either succeeded or failed yet.
-    Failed executions are skipped. """
-    for executionARN in executionARNs:
-        last_event = sfn_client.get_execution_history(
-                executionArn=executionARN,
-                maxResults=1,
-                reverseOrder=True)['events'][0]
 
-        if last_event['type'] == 'ExecutionFailed':
-            _log('[ERROR] Execution {} failed.'.format(executionARN))
-            pass
-        elif last_event['type'] != 'ExecutionSucceeded':
-            return True # Executions still pending
-    _log('All executions finished(failed/succeeded).')
-    return False
+def execute_sfn(state_machine_name, matrix_names, block_size):
+    execution_info = invoke_matrix_multiplication(
+                    state_machine_name=state_machine_name,
+                    executionName=state_machine_name,
+                    name_matrixA=matrix_names[0],
+                    name_matrixB=matrix_names[1],
+                    block_size=block_size)
+    split_executionARNs = [x['executionARN'] for x in execution_info['split-executions']]
+    _log("Waiting for pending executions to finish...")
+    while executions_pending(split_executionARNs):
+        sleep(2)
+
+def invoke_matrix_multiplication(state_machine_name, executionName, name_matrixA, name_matrixB, block_size):
+    """ execution_info.keys() :> ['deploy-nr', 'split-executions', 'split'] """
+    sfn_input = create_input(state_machine_name, executionName, name_matrixA, name_matrixB, block_size)
+    _log("Invoking matrix multiplication with input: {}".format(sfn_input))
+    response = lambda_client.invoke(
+            FunctionName='mmultiply-prod-multi-unit-multiplication',
+            InvocationType='RequestResponse',
+            LogType='Tail',
+            Payload=json.dumps(sfn_input))
+    _log('LogResult: \n{}'.format(base64.b64decode(response['LogResult'])))
+    return json.loads(response['Payload'].read())
 
 def create_input(state_machine_name, executionName, name_matrixA, name_matrixB, block_size):
     input = {
@@ -142,42 +149,28 @@ def create_input(state_machine_name, executionName, name_matrixA, name_matrixB, 
         }
     return input
 
-def invoke_matrix_multiplication(state_machine_name, executionName, name_matrixA, name_matrixB, block_size):
-    """ execution_info.keys() :> ['deploy-nr', 'split-executions', 'split'] """
-    sfn_input = create_input(state_machine_name, executionName, name_matrixA, name_matrixB, block_size)
-    _log("Invoking matrix multiplication with input: {}".format(sfn_input))
-    response = lambda_client.invoke(
-            FunctionName='mmultiply-prod-multi-unit-multiplication',
-            InvocationType='RequestResponse',
-            LogType='Tail',
-            Payload=json.dumps(sfn_input))
-    _log('LogResult: \n{}'.format(base64.b64decode(response['LogResult'])))
-    return json.loads(response['Payload'].read())
+def executions_pending(executionARNs):
+    """ Checks if all executions have either succeeded or failed yet.
+    Failed executions are skipped. """
+    for executionARN in executionARNs:
+        last_event = sfn_client.get_execution_history(
+                executionArn=executionARN,
+                maxResults=1,
+                reverseOrder=True)['events'][0]
 
-def execute_sfn(state_machine_name, matrix_names, block_size):
-    execution_info = invoke_matrix_multiplication(
-                    state_machine_name=state_machine_name,
-                    executionName=state_machine_name,
-                    name_matrixA=matrix_names[0],
-                    name_matrixB=matrix_names[1],
-                    block_size=block_size)
-    split_executionARNs = [x['executionARN'] for x in execution_info['split-executions']]
-    _log("Waiting for pending executions to finish...")
-    while executions_pending(split_executionARNs):
-        sleep(2)
+        if last_event['type'] == 'ExecutionFailed':
+            _log('[ERROR] Execution {} failed.'.format(executionARN))
+            pass
+        elif last_event['type'] != 'ExecutionSucceeded':
+            return True # Executions still pending
+    _log('All executions finished(failed/succeeded).')
+    return False
 
 
 
-def extract_time_profiles(events):
-    """ Time profiles are part of the lambda output which is returned as a json formated string """
-    _log("Extracting time profiles")
-    lambda_outputs = [x['lambdaFunctionSucceededEventDetails']['output'] for x in events if x['type'] == 'LambdaFunctionSucceeded']
-    time_profiles = [json.loads(x)['time-profile'] for x in lambda_outputs]
-
-    time_profiles_by_lambda = defaultdict(list)
-    for profile in time_profiles:
-        time_profiles_by_lambda[profile['lambda']].append(profile)
-
+def parse_time_profiles(executionARNs):
+    events = get_all_events(executionARNs)
+    time_profiles_by_lambda = extract_time_profiles(events)
     return time_profiles_by_lambda
 
 def get_all_events(executionARNs):
@@ -193,12 +186,17 @@ def get_all_events(executionARNs):
                     executionArn=executionARN,
                     nextToken=response['nextToken'])
             events.extend(response['events'])
-
     return events
 
-def parse_time_profiles(executionARNs):
-    events = get_all_events(executionARNs)
-    time_profiles_by_lambda = extract_time_profiles(events)
+def extract_time_profiles(events):
+    """ Time profiles are part of the lambda output which is returned as a json formated string """
+    _log("Extracting time profiles")
+    lambda_outputs = [x['lambdaFunctionSucceededEventDetails']['output'] for x in events if x['type'] == 'LambdaFunctionSucceeded']
+    time_profiles = [json.loads(x)['time-profile'] for x in lambda_outputs]
+
+    time_profiles_by_lambda = defaultdict(list)
+    for profile in time_profiles:
+        time_profiles_by_lambda[profile['lambda']].append(profile)
     return time_profiles_by_lambda
 
 
@@ -215,25 +213,34 @@ def save_raw_data(time_profiles_by_lambda, csv_path):
 
 
 
-def plot_time_distribution(timings, lambda_type, state_machine_name, plot_dir, to_file=True):
-    _log('Creating plot {} distribution {}'.format(state_machine_name, lambda_type))
+def create_plots_per_lambda(csv_path, state_machine_name, to_file=True):
+    for lambda_type in ['intermediate', 'collect', 'accumulate']:
+        lambda_timings = load_timings(csv_path, lambda_type)
+        plot_time_profile(with_average(lambda_timings), lambda_type, state_machine_name, os.path.dirname(csv_path), to_file)
+        plot_time_distribution(lambda_timings, lambda_type, state_machine_name, os.path.dirname(csv_path), to_file)
 
-    up, down, calculation = np.average(timings.up), np.average(timings.down), np.average(timings.calculation)
-    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-    labels = 'S3 upload\n({}ms)'.format(int(up)), 'S3 download\n({}ms)'.format(int(down)), 'calculation\n({}ms)'.format(int(calculation))
-    sizes = [up, down, calculation]
-    explode = (0, 0, 0.1)  # only "explode" the 3nd slice
+def create_combined_plots(csv_path, state_machine_name, to_file=True):
+    combined_timings = load_timings(csv_path)
+    plot_time_profile(combined_timings, 'combined', state_machine_name, os.path.dirname(csv_path), to_file)
+    plot_time_distribution(combined_timings, 'combined', state_machine_name, os.path.dirname(csv_path), to_file)
 
-    fig1, ax = plt.subplots()
-    ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90, colors=['c','b','m'])
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    ax.set_title('Time spent on S3 upload, S3 download and actual calculation in percent.')
+def load_timings(csv_path, lambda_type=None):
+    with open(csv_path, 'r') as f:
+        reader = csv.DictReader(f, delimiter=',')
+        # 'type','down','up','execution'
+        up, down, calculation = [], [], []
+        for row in reader:
+            if lambda_type == None or lambda_type == row['type']:
+                up.append(row['up'])
+                down.append(row['down'])
+                calculation.append(int(row['execution']) - int(row['down']) - int(row['up']))
+    return Timings(np.array(up, dtype=int), np.array(down, dtype=int), np.array(calculation, dtype=int))
 
-    if to_file:
-        plot_path = '{}/{}_{}_dist.png'.format(plot_dir, state_machine_name, lambda_type)
-        plt.savefig(join(BENCHMARKS_FOLDER, plot_path), dpi=500)
-    else:
-        plt.show()
+def with_average(timings):
+    down = np.append(timings.down, np.average(timings.down))
+    up = np.append(timings.up, np.average(timings.up))
+    calculation = np.append(timings.calculation, np.average(timings.calculation))
+    return Timings(down, up, calculation)
 
 def plot_time_profile(timings, lambda_type, state_machine_name, plot_dir=None, to_file=True):
     _log('Creating plot {} profile {}'.format(state_machine_name, lambda_type))
@@ -262,34 +269,25 @@ def plot_time_profile(timings, lambda_type, state_machine_name, plot_dir=None, t
     else:
         plt.show()
 
-def load_timings(csv_path, lambda_type=None):
-    with open(csv_path, 'r') as f:
-        reader = csv.DictReader(f, delimiter=',')
-        # 'type','down','up','execution'
-        up, down, calculation = [], [], []
-        for row in reader:
-            if lambda_type == None or lambda_type == row['type']:
-                up.append(row['up'])
-                down.append(row['down'])
-                calculation.append(int(row['execution']) - int(row['down']) - int(row['up']))
-    return Timings(np.array(up, dtype=int), np.array(down, dtype=int), np.array(calculation, dtype=int))
+def plot_time_distribution(timings, lambda_type, state_machine_name, plot_dir, to_file=True):
+    _log('Creating plot {} distribution {}'.format(state_machine_name, lambda_type))
 
-def with_average(timings):
-    down = np.append(timings.down, np.average(timings.down))
-    up = np.append(timings.up, np.average(timings.up))
-    calculation = np.append(timings.calculation, np.average(timings.calculation))
-    return Timings(down, up, calculation)
+    up, down, calculation = np.average(timings.up), np.average(timings.down), np.average(timings.calculation)
+    # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+    labels = 'S3 upload\n({}ms)'.format(int(up)), 'S3 download\n({}ms)'.format(int(down)), 'calculation\n({}ms)'.format(int(calculation))
+    sizes = [up, down, calculation]
+    explode = (0, 0, 0.1)  # only "explode" the 3nd slice
 
-def create_plots_per_lambda(csv_path, state_machine_name, to_file=True):
-    for lambda_type in ['intermediate', 'collect', 'accumulate']:
-        lambda_timings = load_timings(csv_path, lambda_type)
-        plot_time_profile(with_average(lambda_timings), lambda_type, state_machine_name, os.path.dirname(csv_path), to_file)
-        plot_time_distribution(lambda_timings, lambda_type, state_machine_name, os.path.dirname(csv_path), to_file)
+    fig1, ax = plt.subplots()
+    ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=False, startangle=90, colors=['c','b','m'])
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.set_title('Time spent on S3 upload, S3 download and actual calculation in percent.')
 
-def create_combined_plots(csv_path, state_machine_name, to_file=True):
-    combined_timings = load_timings(csv_path)
-    plot_time_profile(combined_timings, 'combined', state_machine_name, os.path.dirname(csv_path), to_file)
-    plot_time_distribution(combined_timings, 'combined', state_machine_name, os.path.dirname(csv_path), to_file)
+    if to_file:
+        plot_path = '{}/{}_{}_dist.png'.format(plot_dir, state_machine_name, lambda_type)
+        plt.savefig(join(BENCHMARKS_FOLDER, plot_path), dpi=500)
+    else:
+        plt.show()
 
 
 
@@ -326,8 +324,8 @@ if __name__ == "__main__":
 
     block_sizes = [2000]
 
-    _log('benchmark parameters:\n  block_sizes:{}\n  BUCKET:{}\n  PREFIX:{}\n  BENCHMARKS_FOLDER:{}\n  SFN_PREFIX:{}'.format(
-                                    block_sizes, BUCKET, PREFIX, BENCHMARKS_FOLDER, SFN_PREFIX))
+    _log('\nBenchmark Parameters:\n  BUCKET:{}\n  PREFIX:{}\n  BENCHMARKS_FOLDER:{}\n  SFN_PREFIX:{}'.format(
+                                   BUCKET, PREFIX, BENCHMARKS_FOLDER, SFN_PREFIX))
 
     matrix_dimension_sets = [
             # MatrixDimensions(height=2000, width=2000),
@@ -335,8 +333,8 @@ if __name__ == "__main__":
             MatrixDimensions(height=4000, width=4000)#,
             # MatrixDimensions(height=8000, width=8000)
             ]
-    _log('blocksize: {}'.format(block_sizes))
-    _log(str(matrix_dimension_sets))
+    _log('blocksizes: {}'.format(block_sizes))
+    _log('matrix dimensions: {}'.format(matrix_dimension_sets))
 
     for block_size in block_sizes:
         for matrix_dimensions in matrix_dimension_sets:
@@ -369,5 +367,3 @@ if __name__ == "__main__":
 
     # compare_time_profiles()
     LOG_FILE.close()
-
-
