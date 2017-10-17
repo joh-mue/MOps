49b3d4248041fb7306195594dfd07c6f64780aba
joh-mue <yesiamkeen@gmail.com>
2017-10-17 17:24:50 +0200

    COL110, in-memory caching and intermediate clearing of tmp folder to prevent filling up of disk

--- strassen-unit-collector.py
+++ strassen-unit-collector.py
@@ -21,7 +21,7 @@ if platform.system() != 'Darwin': # don't do this on my local machine
 import numpy as np
 ### NUMPY, SCIPY, SKLEARN MAGIC END
 
-deploy_nr = 'COL109'
+deploy_nr = 'COL110'
 
 s3_client = boto3.client('s3')
 
@@ -36,6 +36,8 @@ s3_client = boto3.client('s3')
 '''
 s3_download_time = 0
 cached_files = 0
+loaded_files = 0
+m = {} # in memory cache for intermediate results
 
 def handler(event, context):
     execution_start = context.get_remaining_time_in_millis()
@@ -43,27 +45,47 @@ def handler(event, context):
     global cached_files
     s3_download_time = 0
     cached_files = 0
+    loaded_files = 0
+    m = {}
 
     result = event['result']
     OperationMetaData = namedtuple('OperationMetaData', ['bucket', 'folder', 'split', 'unit'])
     op_meta_data = OperationMetaData(result['bucket'], result['folder'], event['split'], event['unit'])
 
-    X00 = x_0_0(op_meta_data)
-    X01 = x_0_1(op_meta_data)
-    X10 = x_1_0(op_meta_data)
-    X11 = x_1_1(op_meta_data)
-
+    aws.cleanup_tmp()
     base = "S{}_X{}_U{}".format(event['split'], "{}", event['unit'])
+    s3_upload_time = 0
+
+    X = x_0_0(op_meta_data)
     start = context.get_remaining_time_in_millis()
-    aws.write_to_s3(X00, result['bucket'], result['folder'], base.format("00"), s3_client)
-    aws.write_to_s3(X01, result['bucket'], result['folder'], base.format("01"), s3_client)
-    aws.write_to_s3(X10, result['bucket'], result['folder'], base.format("10"), s3_client)
-    aws.write_to_s3(X11, result['bucket'], result['folder'], base.format("11"), s3_client)
+    aws.write_to_s3(X, result['bucket'], result['folder'], base.format("00"), s3_client)
     end = context.get_remaining_time_in_millis()
-    s3_upload_time = start - end
+    s3_upload_time += start - end
+    aws.cleanup_tmp()
+
+    X = x_0_1(op_meta_data)
+    start = context.get_remaining_time_in_millis()
+    aws.write_to_s3(X, result['bucket'], result['folder'], base.format("01"), s3_client)
+    end = context.get_remaining_time_in_millis()
+    s3_upload_time += start - end
+    aws.cleanup_tmp()
+
+    X = x_1_0(op_meta_data)
+    start = context.get_remaining_time_in_millis()
+    aws.write_to_s3(X, result['bucket'], result['folder'], base.format("10"), s3_client)
+    end = context.get_remaining_time_in_millis()
+    s3_upload_time += start - end
+    aws.cleanup_tmp()
+
+
+    X = x_1_1(op_meta_data)
+    start = context.get_remaining_time_in_millis()
+    aws.write_to_s3(X, result['bucket'], result['folder'], base.format("11"), s3_client)
+    end = context.get_remaining_time_in_millis()
+    s3_upload_time += start - end
+
     execution_time = execution_start - context.get_remaining_time_in_millis()
 
-    aws.cleanup_tmp()
     return {
             'time-profile': {
                 's3-up': s3_upload_time,
@@ -73,47 +95,51 @@ def handler(event, context):
             },
             'deploy-nr': deploy_nr,
             'remaining_time_at_exec_start': execution_start,
-            'cached-files': cached_files
+            'cached-files': cached_files,
+            'loaded-files': loaded_files
     }
 
 
 def load_interm_result(op_meta_data, x):
     global s3_download_time
     global cached_files
+    global loaded_files
 
     filename = 'S{}_U{}_m{}'.format(op_meta_data.split, op_meta_data.unit, x)
     path = os.path.join('/tmp/', op_meta_data.folder, filename)
-    if not os.path.exists(path):
+    if os.path.exists(path) | m.has_key(x):
+        cached_files += 1
+        return m[x]
+    else:
         start = time.time()
         path = aws.download_s3_file(op_meta_data.bucket, op_meta_data.folder, filename, s3_client)
         end = time.time()
         s3_download_time = s3_download_time + int((end - start) * 1000)
-    else:
-        cached_files += 1
+        loaded_files += 1
 
     return np.load(path)
 
 
 def x_0_0(op_meta_data):
-    m_0 = load_interm_result(op_meta_data, 0)
-    m_3 = load_interm_result(op_meta_data, 3)
-    m_4 = load_interm_result(op_meta_data, 4)
-    m_6 = load_interm_result(op_meta_data, 6)
-    return m_0 + m_3 - m_4 + m_6
+    m[0] = load_interm_result(op_meta_data, 0)
+    m[3] = load_interm_result(op_meta_data, 3)
+    m[4] = load_interm_result(op_meta_data, 4)
+    m[6] = load_interm_result(op_meta_data, 6)
+    return m[0] + m[3] - m[4] + m[6]
 
 def x_0_1(op_meta_data):
-    m_2 = load_interm_result(op_meta_data, 2)
-    m_4 = load_interm_result(op_meta_data, 4)
-    return m_2 + m_4
+    m[2] = load_interm_result(op_meta_data, 2)
+    m[4] = load_interm_result(op_meta_data, 4)
+    return m[2] + m[4]
 
 def x_1_0(op_meta_data):
-    m_1 = load_interm_result(op_meta_data, 1)
-    m_3 = load_interm_result(op_meta_data, 3)
-    return m_1 + m_3
+    m[1] = load_interm_result(op_meta_data, 1)
+    m[3] = load_interm_result(op_meta_data, 3)
+    return m[1] + m[3]
 
 def x_1_1(op_meta_data):
-    m_0 = load_interm_result(op_meta_data, 0)
-    m_2 = load_interm_result(op_meta_data, 2)
-    m_1 = load_interm_result(op_meta_data, 1)
-    m_5 = load_interm_result(op_meta_data, 5)
-    return m_0 + m_2 - m_1 + m_5
+    m[0] = load_interm_result(op_meta_data, 0)
+    m[2] = load_interm_result(op_meta_data, 2)
+    m[1] = load_interm_result(op_meta_data, 1)
+    m[5] = load_interm_result(op_meta_data, 5)
+    return m[0] + m[2] - m[1] + m[5]
